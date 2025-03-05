81ebd0f5908ede0c5505fb46152220610a00a186
Dominic Galati <Dominic_Galati@insideranken.org>
2025-03-04 22:47:56 -0600

    Projects
    Projects

--- WebPortfolio/MyPortfolio/Controllers/HomeController.cs
+++ WebPortfolio/MyPortfolio/Controllers/HomeController.cs
@@ -6,26 +6,23 @@ namespace MyPortfolio.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Projects()
     {
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    public IActionResult AboutMe()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
     }
 }
--- WebPortfolio/MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ WebPortfolio/MyPortfolio/obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07422876964eba7fc19f21800eb070a8abe5b68b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
